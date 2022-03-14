b2a168d527365f89340bee4d51a0681ea92b8841
Rui Vilela <ruivil@gmail.com>
2022-03-14 17:56:06 +0000

    C# script

---
+++ CSharpRelated/HelloWorldLikePy/Program.cs
@@ -0,0 +1,8 @@
+// Hello world like in Python
+// dotnet run
+
+Console.WriteLine("Hello World!");
+Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+Console.WriteLine($"Hello, {name}!");
+
